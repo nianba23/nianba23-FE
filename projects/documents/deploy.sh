43a87026fd26ab32db3eebb49a469dd8ba096ad8
changhua0118 <changhua0118@163.com>
2023-12-02 21:29:29 +0800

    update vitepress deploy.sh

--- projects/documents/deploy.sh
+++ projects/documents/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 构建
-npm run docs:build
+npm run build
 
 # 进入待发布的目录
 cd docs/.vitepress/dist
@@ -18,7 +18,7 @@ git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@github.com:nianba23/nianba23.github.io.git main
+git push -f git@github.com:nianba23/nianba23.github.io.git master:main
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
